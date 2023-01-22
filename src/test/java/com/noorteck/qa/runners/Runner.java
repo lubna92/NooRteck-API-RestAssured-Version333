676b8f4e7590c2006a6508282ad761dbac805be7
Lubna Sami <lublub954@gmail.com>
2023-01-21 22:47:43 -0500

    target changed

--- src/test/java/com/noorteck/qa/runners/Runner.java
+++ src/test/java/com/noorteck/qa/runners/Runner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 					features = "src/test/resources/hr/",
 					glue = "com.noorteck.qa.steps",					
 							plugin = {"summary","pretty", "html:target/cucumber-reports.html",
-									"json:target/cucumber-reports",					
+									"json:target/desination/cucumber-reports.json",					
 									"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 					monochrome = true
 						)
